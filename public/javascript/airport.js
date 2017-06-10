144619cd575344b65a582af54bd20174a5be3a5c
Edward Goo <Edwardx89@yahoo.com>
2017-06-10 09:38:38 -0400

    fixed indentation

--- public/javascript/airport.js
+++ public/javascript/airport.js
@@ -1,95 +1,93 @@
-$( document ).ready(function() {
-  //autocomplete functionalities using air-port-codes API
-  $(function() {
-      //going through all of the autocomplete classes
-    $( '.autocomplete' ).each(function () {
-        let apca = new apc('autocomplete', {
-            key : '2b19bd2446',
-            secret : 'bc4ed20725a3d97', // Your API Secret Key: use this if you are not connecting from a web server
-            limit : 7
-        });
-
-        let dataObj = {
-            source: function( request, response ) {
-                // make the request
-                apca.request( request.term );
-
-                // this builds each line of the autocomplete
-                itemObj = function (airport, isChild) {
-                    let label;
-                    if (isChild) { // format children labels to show all airports
-                        label = '&rdsh;' + airport.iata + ' - ' + airport.name;
-                    } else { // format labels
-                        label = airport.city;
-                        if (airport.state.abbr) {
-                            label += ', ' + airport.state.abbr;
-                        }
-                        label += ', ' + airport.country.name;
-                        label += ' (' + airport.iata + ' - ' + airport.name + ')';
-                    }
-                    return {
-                        label: label,
-                        value: airport.iata + ' - ' + airport.name,
-                        code: airport.iata
-                    };
-                };
-
-                // this deals with the successful response data
-                apca.onSuccess = function (data) {
-                  var listAry = [],
-                    thisAirport;
-                  if (data.status) { // success
-                    for (var i = 0, len = data.airports.length; i < len; i++) {
-                      thisAirport = data.airports[i];
-                      //set this condition to filter only for United States airports
-                      if(thisAirport.country.name === 'United States') listAry.push(itemObj(thisAirport, true));
-                      if (thisAirport.children && thisAirport.country.name === 'United States') {
-                        for (var j = 0, jLen = thisAirport.children.length; j < jLen; j++) {
-                          listAry.push(itemObj(thisAirport.children[j], true));
-                        }
-                      }
-                    }
-                    response(listAry);
-                    } else { // no results
-                        response();
-                    }
-                };
-                //error handling
-                apca.onError = function (data) {
-                    response();
-                    console.log(data.message);
-                };
-            },
-            //The select event handler.
-            select: function( event, ui ) {
-                //get the information of the selected airport from the API
-                let $name = $(this)['0'].name
-                let $thisAirport = $(this)
-                apcs = new apc('single', {
-                  key: '2b19bd2446',
-                  secret: 'bc4ed20725a3d97', // Your API Secret Key: use this if you are not connecting from a web server
-                  limit: 3
-                });
-                //making the API call with the airport code.
-                apcs.request(ui.item.code)
-
-                //error handler
-                apcs.onError = function (data) {
-                  console.log(data.message);
-                };
-                //if successful run the function.
-                apcs.onSuccess = function (data) {
-                  //attaching data.airport to the dom
-                  $thisAirport.data(data.airport)
-                };
-            }
-        }
-
-        // this is necessary to allow html entities to display properly in the jqueryUI labels
-        $(this).autocomplete(dataObj).data("ui-autocomplete")._renderItem = function( ul, item) {
-            return $('<li></li>').data('item.autocomplete', item ).html( item.label ).appendTo( ul );
-        };
+//autocomplete functionalities using air-port-codes API
+$(function () {
+  //going through all of the autocomplete classes
+  $('.autocomplete').each(function () {
+    let apca = new apc('autocomplete', {
+      key: '2b19bd2446',
+      secret: 'bc4ed20725a3d97', // Your API Secret Key: use this if you are not connecting from a web server
+      limit: 7
     });
-});
+
+    let dataObj = {
+      source: function (request, response) {
+        // make the request
+        apca.request(request.term);
+
+        // this builds each line of the autocomplete
+        itemObj = function (airport, isChild) {
+          let label;
+          if (isChild) { // format children labels to show all airports
+            label = '&rdsh;' + airport.iata + ' - ' + airport.name;
+          } else { // format labels
+            label = airport.city;
+            if (airport.state.abbr) {
+              label += ', ' + airport.state.abbr;
+            }
+            label += ', ' + airport.country.name;
+            label += ' (' + airport.iata + ' - ' + airport.name + ')';
+          }
+          return {
+            label: label,
+            value: airport.iata + ' - ' + airport.name,
+            code: airport.iata
+          };
+        };
+
+        // this deals with the successful response data
+        apca.onSuccess = function (data) {
+          var listAry = [],
+            thisAirport;
+          if (data.status) { // success
+            for (var i = 0, len = data.airports.length; i < len; i++) {
+              thisAirport = data.airports[i];
+              //set this condition to filter only for United States airports
+              if (thisAirport.country.name === 'United States') listAry.push(itemObj(thisAirport, true));
+              if (thisAirport.children && thisAirport.country.name === 'United States') {
+                for (var j = 0, jLen = thisAirport.children.length; j < jLen; j++) {
+                  listAry.push(itemObj(thisAirport.children[j], true));
+                }
+              }
+            }
+            response(listAry);
+          } else { // no results
+            response();
+          }
+        };
+        //error handling
+        apca.onError = function (data) {
+          response();
+          console.log(data.message);
+        };
+      },
+      //The select event handler.
+      select: function (event, ui) {
+        //get the information of the selected airport from the API
+        let $name = $(this)['0'].name
+        let $thisAirport = $(this)
+        apcs = new apc('single', {
+          key: '2b19bd2446',
+          secret: 'bc4ed20725a3d97', // Your API Secret Key: use this if you are not connecting from a web server
+          limit: 3
+        });
+        //making the API call with the airport code.
+        apcs.request(ui.item.code)
+
+        //error handler
+        apcs.onError = function (data) {
+          console.log(data.message);
+        };
+        //if successful run the function.
+        apcs.onSuccess = function (data) {
+          //attaching data.airport to the dom
+          $thisAirport.data(data.airport)
+        };
+      }
+    }
+
+    // this is necessary to allow html entities to display properly in the jqueryUI labels
+    $(this).autocomplete(dataObj).data("ui-autocomplete")._renderItem = function (ul, item) {
+      return $('<li></li>').data('item.autocomplete', item).html(item.label).appendTo(ul);
+    };
+  });
 });
 
--- public/javascript/maps.js
+++ public/javascript/maps.js
@@ -1,36 +1,35 @@
 //This will initiate the google map api.
 let map;
- function initMap (airport1, airport2) {
-         map = new google.maps.Map(document.getElementById('map'), {
-          zoom: 5,
-          center: {lat: 37.0902, lng: -95.7129},
-        });
- }
+function initMap(airport1, airport2) {
+  map = new google.maps.Map(document.getElementById('map'), {
+    zoom: 5,
+    center: { lat: 37.0902, lng: -95.7129 },
+  });
+}
 
 //This will draw a path between two airports.
-  function drawPath(airport1, airport2) {
-    let airport1path = new google.maps.LatLng(airport1.lat, airport1.lng)
-    let airport2path = new google.maps.LatLng(airport2.lat, airport2.lng)
-    //creating the paths and storing it.
-    const paths = new google.maps.Polyline({
-      geodesic: true,
-      path: [airport1path, airport2path],
-      strokeColor: "#FF0000",
-      strokeOpacity: 1.0,
-      strokeWeight: 2
-    });
-    //creating the bound between two airports
-    let bounds = setBound([airport1path, airport2path]);
-    //set the paths on the map
-    paths.setMap(map);
-    //fix the view of the map based on the bounds
-    map.fitBounds(bounds)
-    return paths;
+function drawPath(airport1, airport2) {
+  let airport1path = new google.maps.LatLng(airport1.lat, airport1.lng)
+  let airport2path = new google.maps.LatLng(airport2.lat, airport2.lng)
+  //creating the paths and storing it.
+  const paths = new google.maps.Polyline({
+    geodesic: true,
+    path: [airport1path, airport2path],
+    strokeColor: "#FF0000",
+    strokeOpacity: 1.0,
+    strokeWeight: 2
+  });
+  //creating the bound between two airports
+  let bounds = setBound([airport1path, airport2path]);
+  //set the paths on the map
+  paths.setMap(map);
+  //fix the view of the map based on the bounds
+  map.fitBounds(bounds)
+  return paths;
 }
 
 //setting the size and zoom of the map
 function setBound(location) {
-  console.log(location)
   let coordinates = location
   let bounds = new google.maps.LatLngBounds();
   for (let i = 0; i < coordinates.length; i++) {
